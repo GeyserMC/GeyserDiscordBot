ffadd4b4b2c1e3863b35df23792965282db7ce3d
Heath123 <heath.mitchell27@gmail.com>
2020-07-24 19:30:07 +0100

    Comment out console log that could crash the bot
    These don't exist in the example .env file so setting it up according to that wouldn't work

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function initialiseModule (name) {
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
-  console.log(`Add me using https://discord.com/oauth2/authorize?client_id=${process.env.BOT_ID}&scope=bot&permissions=${process.env.BOT_PERMISSION}`)
+  // console.log(`Add me using https://discord.com/oauth2/authorize?client_id=${process.env.BOT_ID}&scope=bot&permissions=${process.env.BOT_PERMISSION}`)
 })
 
 initialiseModule('dump_analyse')
