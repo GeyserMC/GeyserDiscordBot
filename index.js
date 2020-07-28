82b40cbe036a6090f3ba175cbead7696b26ef8b2
rtm516 <ryantmilner@hotmail.co.uk>
2020-07-29 00:22:34 +0100

    Add comments, editing groups and some general cleanup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
 const Discord = require('discord.js')
 const client = new Discord.Client()
+
+// Load in the .env file
+require('dotenv').config()
 
 function initialiseModule (name) {
   return require('./bot_modules/' + name + '/index.js').init(client, config)
@@ -10,7 +12,10 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+// Load the config
 const config = require('./bot_modules/config_manager/index.js').init(client)
+
+// Load the modules
 initialiseModule('dump_analyse')
 
 client.login(process.env.BOT_TOKEN)
