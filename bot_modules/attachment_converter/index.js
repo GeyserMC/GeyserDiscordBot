6f3d25745a71094fa7ea3abde60b7a2365e367be
rtm516 <ryantmilner@hotmail.co.uk>
2020-08-20 15:30:29 +0100

    Added size limitation to prevent errors from hastebin

--- bot_modules/attachment_converter/index.js
+++ bot_modules/attachment_converter/index.js
@@ -8,16 +8,22 @@ exports.init = (client) => {
   client.on('message', async (msg) => {
     msg.attachments.forEach(async (attachment) => {
       if (config.get().convertExtensions.includes(path.extname(attachment.name))) {
-        const contents = await getAttachmentContents(attachment.url)
-
-        const hastebinPost = await postToHastebin(contents)
-        const hastebinUrl = 'https://hastebin.com/' + hastebinPost.key
-
         const embed = new Discord.MessageEmbed()
-        embed.setColor(0x00ff00)
-        embed.setTitle(hastebinUrl)
-        embed.setURL(hastebinUrl)
-        embed.setDescription(`Converted \`${attachment.name}\` to a hastebin link!`)
+        if (attachment.size > 400000) {
+          embed.setColor(0xff0000)
+          embed.setTitle('Hastebin upload failed!')
+          embed.setDescription('The message.txt was larger than the max size hastebin allows')
+        } else {
+          const contents = await getAttachmentContents(attachment.url)
+
+          const hastebinPost = await postToHastebin(contents)
+          const hastebinUrl = 'https://hastebin.com/' + hastebinPost.key
+
+          embed.setColor(0x00ff00)
+          embed.setTitle(hastebinUrl)
+          embed.setURL(hastebinUrl)
+          embed.setDescription(`Converted \`${attachment.name}\` to a hastebin link!`)
+        }
 
         msg.channel.send(embed)
       }
