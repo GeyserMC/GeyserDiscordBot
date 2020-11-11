ffeaf4d37c9c47e5dda6d1dbf3146ed2fc187252
rtm516 <ryantmilner@hotmail.co.uk>
2020-11-11 20:53:50 +0000

    Add tag loader along with base commands and an example tag

--- bot_modules/tags/loader.js
+++ bot_modules/tags/loader.js
@@ -1,5 +1,66 @@
+const path = require('path')
 const fs = require('fs')
 
+// Relative to the project root
+const tagFolder = './tags'
+
 exports.load = () => {
-  // Read from file...
+  const tags = {}
+
+  // Read tags from files
+  fs.readdirSync(tagFolder).forEach(file => {
+    if (!file.endsWith('.tag')) {
+      return
+    }
+
+    // Read the tag contents
+    const content = fs.readFileSync(path.join(tagFolder, file), { encoding: 'utf-8' })
+
+    // Get the name and set the base tag
+    const tagName = file.substr(0, file.length - 4)
+    const tag = {
+      type: 'text',
+      target: '',
+      content: ''
+    }
+
+    // Loop each line
+    let hitSeperator = false
+    content.split('\n').forEach(line => {
+      line = line.trim()
+
+      // Have we hit the contents yet and if so just append
+      if (hitSeperator) {
+        tag.content += line + '\n'
+        return
+      }
+
+      // Check for the seperator
+      if (line === '---') {
+        hitSeperator = true
+      } else if (line.length !== 0 && !line.startsWith('#')) {
+        // Parse the tag options
+        const lineParts = line.split(':')
+        switch (lineParts[0]) {
+          case 'type':
+            tag.type = lineParts[1].trim()
+            break
+
+          case 'target':
+            tag.target = lineParts[1].trim()
+            break
+
+          default:
+            console.error(`Invalid tag option '${lineParts[0]}' for tag '${tagName}'`)
+            break
+        }
+      }
+    })
+
+    tag.content = tag.content.trim()
+
+    tags[tagName] = tag
+  })
+
+  return tags
 }
--- bot_modules/tags/tags.js
+++ bot_modules/tags/tags.js
@@ -2,18 +2,87 @@ const Discord = require('discord.js')
 const loader = require('./loader')
 
 let tags = {}
+let tagList
 
 exports.initTags = () => {
   // Load tags from file
   tags = loader.load()
+
+  // Cache the list of tag names
+  tagList = Object.keys(tags)
+
+  // Check all aliases are valid
+  tagList.forEach(tagName => {
+    const tag = tags[tagName]
+    if (tag.type === 'alias') {
+      let reason = ''
+      if (!tagList.includes(tag.target)) {
+        reason = 'missing, ' + tag.target
+      } else if (tags[tag.target].type === 'alias') {
+        reason = 'alias'
+      }
+
+      if (reason !== '') {
+        console.error(`The alias tag '${tagName}' has an invalid target (${reason})`)
+      }
+    }
+  })
+
+  console.log(`Loaded ${tagList.length} tags`)
 }
 
 exports.handleTagsCommand = async (msg, args) => {
   // Print all tags
   // Ignore args, unless we want search?
+  const embed = new Discord.MessageEmbed()
+
+  const tagNameList = []
+  tagList.forEach(tagName => {
+    if (tags[tagName].type !== 'alias') {
+      tagNameList.push(tagName)
+    }
+  })
+
+  embed.setColor(0x00ff00)
+  embed.setTitle(`Tags (${tagNameList.length})`)
+  embed.setDescription(`\`${tagNameList.join(', ')}\``)
+  embed.setFooter('Use "!tag name" to show a tag')
+
+  msg.channel.send(embed)
 }
 
 exports.handleTagCommand = async (msg, args) => {
-  // Print a specific tag
-  // Only handle first arg
+  // Check we were sent a tag
+  if (args.length <= 1) {
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('Invalid usage')
+    embed.setDescription('Missing tag name. `!tag <name>`')
+    embed.setColor(0xff0000)
+    msg.channel.send(embed)
+    return
+  }
+
+  // Get the tag name
+  const tagName = args[1]
+
+  // Check if the tag exists
+  if (!tagList.includes(tagName)) {
+    const embed = new Discord.MessageEmbed()
+    embed.setTitle('Missing tag')
+    embed.setDescription(`No tag with the name \`${tagName}\`, do \`!tags\` for the full list.\nAdd a search of possible ones here? (Using the same simmilarity check as the provider command)`)
+    embed.setColor(0xff0000)
+    msg.channel.send(embed)
+    return
+  }
+
+  // Get the tag and contents
+  const tag = tags[tagName]
+  let content = tag.content
+
+  // Get the target contents if its an alias
+  if (tag.type === 'alias') {
+    content = tags[tag.target].content
+  }
+
+  msg.channel.send(content)
 }
