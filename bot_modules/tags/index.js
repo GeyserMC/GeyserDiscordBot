f43b7e1f44fcc08b76e96a301116927e21e89100
rtm516 <ryantmilner@hotmail.co.uk>
2020-11-11 23:50:38 +0000

    Add tag shortcut command

--- bot_modules/tags/index.js
+++ bot_modules/tags/index.js
@@ -6,6 +6,14 @@ exports.init = async (client) => {
   client.on('message', async (msg) => {
     const args = msg.content.split(' ')
 
+    // Allow for tags to be called with the prefix `!!`
+    if (args[0].match(/^!![\w-]+$/)) {
+      const newArgs = ['!tag', ...args]
+      newArgs[1] = newArgs[1].substr(2)
+      await tags.handleTagCommand(msg, newArgs)
+      return
+    }
+
     // Run the relevent function for the command
     switch (args[0]) {
       case '!tags':
