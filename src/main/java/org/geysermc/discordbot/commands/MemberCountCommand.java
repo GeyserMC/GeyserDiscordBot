04ccd7fe2caa20ed61add53bff72d210c715f216
rtm516 <ryantmilner@hotmail.co.uk>
2021-02-01 23:40:16 +0000

    Add member count command

---
+++ src/main/java/org/geysermc/discordbot/commands/MemberCountCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2020-2021 GeyserMC. http://geysermc.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @author GeyserMC
+ * @link https://github.com/GeyserMC/GeyserDiscordBot
+ */
+
+package org.geysermc.discordbot.commands;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import org.geysermc.discordbot.GeyserBot;
+import org.geysermc.discordbot.listeners.SwearHandler;
+import org.geysermc.discordbot.storage.LevelInfo;
+import org.geysermc.discordbot.util.BotHelpers;
+
+import java.awt.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MemberCountCommand extends Command {
+
+    public MemberCountCommand() {
+        this.name = "membercount";
+        this.help = "Show the current member count";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        if (SwearHandler.filteredMessages.contains(event.getMessage().getIdLong())) {
+            return;
+        }
+
+        event.getMessage().reply(new EmbedBuilder()
+                .addField("Members", String.valueOf(event.getGuild().getMemberCount()), false)
+                .setTimestamp(Instant.now())
+                .setColor(Color.green)
+                .build()).queue();
+    }
+}
