0b3c629c6e5b86f5c0d2c6f41e61910ed11ea15c
higbead <higbead@pm.me>
2021-03-13 18:43:51 -0600

    Fix failed linting check

--- bot_modules/autopublish/index.js
+++ bot_modules/autopublish/index.js
@@ -1,14 +1,6 @@
-const { Client } = require('discord.js')
-
-/**
- * @type {Array}
- */
 const { configEditor: config } = require('../config_manager/index.js')
 
-/**
- * @param {Client} client 
- */
-exports.init = function(client) {
+exports.init = function (client) {
   client.on('message', msg => {
     if (config.get().autopublishChannels.includes(msg.channel.id) && msg.crosspostable) {
       msg.crosspost()
