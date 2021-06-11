20ec7e0f2f791dda7992db51c2d6e3d228296dfe
rtm516 <ryantmilner@hotmail.co.uk>
2021-06-11 21:40:57 +0100

    Add role filter to the member count

--- src/main/java/org/geysermc/discordbot/commands/MemberCountCommand.java
+++ src/main/java/org/geysermc/discordbot/commands/MemberCountCommand.java
@@ -30,36 +30,48 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.geysermc.discordbot.util.BotHelpers;
 import org.geysermc.discordbot.util.PropertiesManager;
 
 import java.time.Instant;
+import java.util.Collections;
 
-/**
- * TODO: Add role filter
- */
 public class MemberCountCommand extends SlashCommand {
 
     public MemberCountCommand() {
         this.name = "membercount";
         this.aliases = new String[] {"members"};
+        this.arguments = "[role]";
         this.help = "Show the current member count";
+
+        this.options = Collections.singletonList(
+                new OptionData(OptionType.ROLE, "role", "The role you want to filter for")
+        );
     }
 
 
     @Override
     protected void execute(SlashCommandEvent event) {
-        event.replyEmbeds(handle(event.getGuild())).queue();
+        Role role = null;
+        if (event.getOption("role") != null) {
+            role = event.getOption("role").getAsRole();
+        }
+
+        event.replyEmbeds(handle(event.getGuild(), role)).queue();
     }
 
     @Override
     protected void execute(CommandEvent event) {
-        event.getMessage().reply(handle(event.getGuild())).queue();
+        event.getMessage().reply(handle(event.getGuild(), BotHelpers.getRole(event.getGuild(), event.getArgs()))).queue();
     }
 
-    protected MessageEmbed handle(Guild guild) {
+    protected MessageEmbed handle(Guild guild, Role role) {
         return new EmbedBuilder()
-                .addField("Members", String.valueOf(guild.getMemberCount()), false)
+                .addField("Members" + (role != null ? " in " + role.getName() : ""), String.valueOf(role != null ? guild.getMembersWithRoles(role).size() : guild.getMemberCount()), false)
                 .setTimestamp(Instant.now())
                 .setColor(PropertiesManager.getDefaultColor())
                 .build();
