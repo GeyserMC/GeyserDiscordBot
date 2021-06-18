78d1c5cf417e153bded664770abf6292745fd6d2
rtm516 <ryantmilner@hotmail.co.uk>
2021-06-18 09:32:29 +0100

    Add new version cross posting

--- src/main/java/org/geysermc/discordbot/updates/UpdateManager.java
+++ src/main/java/org/geysermc/discordbot/updates/UpdateManager.java
@@ -26,7 +26,9 @@
 package org.geysermc.discordbot.updates;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.exceptions.InsufficientPermissionException;
 import org.geysermc.discordbot.GeyserBot;
 import org.geysermc.discordbot.storage.ServerSettings;
 import org.json.JSONException;
@@ -92,7 +94,11 @@ public class UpdateManager {
      */
     public static void sendMessage(String message) {
         for (TextChannel updateChannel : UPDATE_CHANNELS) {
-            updateChannel.sendMessage(message).complete().crosspost().queue();
+            Message msg = updateChannel.sendMessage(message).complete();
+            // Crosspost or silently fail
+            try {
+                msg.crosspost().queue();
+            } catch (UnsupportedOperationException | IllegalStateException | InsufficientPermissionException ignored) {}
         }
     }
 }
