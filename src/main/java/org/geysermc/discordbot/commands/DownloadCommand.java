52eeb032d60b2a1f0e04a7d2c4520a0f94cadf25
rtm516 <ryantmilner@hotmail.co.uk>
2021-06-18 17:48:47 +0100

    Add a classic handler for the download command

--- src/main/java/org/geysermc/discordbot/commands/DownloadCommand.java
+++ src/main/java/org/geysermc/discordbot/commands/DownloadCommand.java
@@ -1,6 +1,7 @@
 package org.geysermc.discordbot.commands;
 
 import com.google.common.collect.ImmutableMap;
+import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
@@ -8,7 +9,6 @@ import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.geysermc.discordbot.util.BotColors;
-import org.geysermc.discordbot.util.PropertiesManager;
 
 import java.util.*;
 
@@ -18,6 +18,7 @@ public class DownloadCommand extends SlashCommand {
 
     public DownloadCommand() {
         this.name = "download";
+        this.arguments = "[program]";
         this.help = "Sends a link to download the latest version of Geyser or another program";
         this.guildOnly = false;
 
@@ -42,6 +43,25 @@ public class DownloadCommand extends SlashCommand {
                         .setRequired(false)
                         .addChoices(choices)
         );
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        List<String> args = new ArrayList<>(Arrays.asList(event.getArgs().split(" ")));
+
+        String program = "geyser";
+        if (!args.get(0).isEmpty()) {
+            program = args.get(0);
+        }
+
+        DownloadOption downloadOption = optionsToRepository.getOrDefault(program.toLowerCase(Locale.ROOT), this.defaultDownloadOption);
+
+        event.getMessage().reply(new EmbedBuilder()
+                .setTitle("Download " + downloadOption.friendlyName)
+                .setDescription("Download at " + downloadOption.downloadUrl)
+                .setThumbnail(downloadOption.imageUrl)
+                .setColor(BotColors.SUCCESS.getColor())
+                .build()).queue();
     }
 
     @Override
