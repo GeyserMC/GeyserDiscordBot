c53501aa223a83f7cf017b27e3588a7c90badc12
Chew <chew@chew.pw>
2022-01-21 18:38:24 -0600

    Remove select menu reference

--- src/main/java/org/geysermc/discordbot/listeners/InteractionHandler.java
+++ src/main/java/org/geysermc/discordbot/listeners/InteractionHandler.java
@@ -25,27 +25,19 @@
 package org.geysermc.discordbot.listeners;
 
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
-import net.dv8tion.jda.api.events.interaction.SelectionMenuEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Class to listen to and delegate interaction (button or selection menu) responses
  */
-@SuppressWarnings({"SwitchStatementWithTooFewBranches", "StatementWithEmptyBody"})
+@SuppressWarnings({"SwitchStatementWithTooFewBranches"})
 public class InteractionHandler extends ListenerAdapter {
     @Override
     public void onButtonClick(@NotNull ButtonClickEvent event) {
         // Handle button clicks here
         switch (event.getComponentId()) {
             case "listeners:error_analyzer:clear" -> ErrorAnalyzer.clearMessage(event);
-        }
-    }
-
-    @Override
-    public void onSelectionMenu(@NotNull SelectionMenuEvent event) {
-        // Handle selection menu choices here
-        switch (event.getComponentId()) {
         }
     }
 }
