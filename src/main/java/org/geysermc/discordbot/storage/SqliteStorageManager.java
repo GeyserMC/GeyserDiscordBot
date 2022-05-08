a35cc7bfe8f60f1e690a6d8fc308c463fdf33eea
7man7LMYT <67489949+7man7LMYT@users.noreply.github.com>
2022-05-08 16:51:10 -0700

    Fix SQLite support (#226)
    * Fix SQLite support
    
    i tested it and it still works so like
    
    * Optimize Imports

--- src/main/java/org/geysermc/discordbot/storage/SqliteStorageManager.java
+++ src/main/java/org/geysermc/discordbot/storage/SqliteStorageManager.java
@@ -25,12 +25,22 @@
 
 package org.geysermc.discordbot.storage;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import org.geysermc.discordbot.GeyserBot;
 import org.geysermc.discordbot.util.PropertiesManager;
+import org.geysermc.discordbot.util.UserById;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqliteStorageManager extends MySQLStorageManager {
 
@@ -50,5 +60,220 @@ public class SqliteStorageManager extends MySQLStorageManager {
         } catch (ClassNotFoundException | SQLException e) {
             GeyserBot.LOGGER.error("Unable to create sqlite database!", e);
         }
+    }
+
+    @Override
+    public void closeStorage() {
+        try {
+            connection.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public String getServerPreference(long serverID, String preference) {
+        try {
+            Statement getPreferenceValue = connection.createStatement();
+            ResultSet rs = getPreferenceValue.executeQuery("SELECT `value` FROM `preferences` WHERE `server`=" + serverID + " AND `key`='" + preference + "';");
+
+            if (rs.next()) {
+                return rs.getString("value");
+            }
+
+            getPreferenceValue.close();
+        } catch (SQLException ignored) { }
+
+        return null;
+    }
+
+    @Override
+    public void setServerPreference(long serverID, String preference, String value) {
+        try {
+            Statement updatePreferenceValue = connection.createStatement();
+            updatePreferenceValue.executeUpdate("INSERT OR REPLACE INTO `preferences` (`server`, `key`, `value`) VALUES ('" + serverID + "', '" + preference + "', '" + value + "');");
+            updatePreferenceValue.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public void addPersistentRole(Member member, Role role) {
+        try {
+            Statement addPersistentRole = connection.createStatement();
+            addPersistentRole.executeUpdate("INSERT OR REPLACE INTO `persistent_roles` (`server`, `user`, `role`) VALUES (" + member.getGuild().getId() + ", " + member.getId() + ", " + role.getId() + ");");
+            addPersistentRole.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public void removePersistentRole(Member member, Role role) {
+        try {
+            Statement removePersistentRole = connection.createStatement();
+            removePersistentRole.executeUpdate("DELETE FROM `persistent_roles` WHERE `server`=" + member.getGuild().getId() + " AND `user`=" + member.getId() + " AND `role`=" + role.getId() + ");");
+            removePersistentRole.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public List<Role> getPersistentRoles(Member member) {
+        List<Role> roles = new ArrayList<>();
+
+        try {
+            Statement getPersistentRoles = connection.createStatement();
+            ResultSet rs = getPersistentRoles.executeQuery("SELECT `role` FROM `persistent_roles` WHERE `server`=" + member.getGuild().getId() + " AND `user`=" + member.getId() + ";");
+
+            while (rs.next()) {
+                roles.add(member.getGuild().getRoleById(rs.getString("role")));
+            }
+
+            getPersistentRoles.close();
+        } catch (SQLException ignored) { }
+
+        return roles;
+    }
+
+    @Override
+    public int addLog(Member user, String action, User target, String reason) {
+        try {
+            Statement addLogEntry = connection.createStatement();
+            long time = Instant.now().getEpochSecond();
+            addLogEntry.executeUpdate("INSERT OR REPLACE INTO `mod_log` (`server`, `time`, `user`, `action`, `target`, `reason`) VALUES (" + user.getGuild().getId() + ", " + time + ", " + user.getId() + ", '" + action + "', " + target.getId() + ", '" + reason + "');");
+            addLogEntry.close();
+
+            Statement getLogEntry = connection.createStatement();
+            ResultSet rs = getLogEntry.executeQuery("SELECT `id` FROM `mod_log` WHERE `server`=" + user.getGuild().getId() + " AND `time`=" + time + " AND `user`=" + user.getId() + " AND `action`='" + action + "' AND `target`=" + target.getId() + " AND `reason`='" + reason + "' LIMIT 1;");
+
+            if (rs.next()) {
+                return rs.getInt("id");
+            }
+
+            getLogEntry.close();
+        } catch (SQLException ignored) { }
+
+        return -1;
+    }
+
+    @Override
+    public List<ModLog> getLogs(Guild guild, User target, int limit) {
+        List<ModLog> logs = new ArrayList<>();
+
+        try {
+            Statement getLogEntry = connection.createStatement();
+            ResultSet rs = getLogEntry.executeQuery("SELECT `id`, `time`, `user`, `action`, `reason` FROM `mod_log` WHERE `server`=" + guild.getId() + " AND `target`=" + target.getId() + " ORDER BY `time` ASC LIMIT " + limit + ";");
+
+            while (rs.next()) {
+                Instant time = Instant.ofEpochSecond(rs.getLong("time"));
+                Member user = guild.getMemberById(rs.getLong("user"));
+
+                logs.add(new ModLog(rs.getInt("id"), time, user, rs.getString("action"), target, rs.getString("reason")));
+            }
+
+            getLogEntry.close();
+        } catch (SQLException ignored) { }
+
+        return logs;
+    }
+
+    @Override
+    public ModLog getLog(Guild guild, int id) {
+        try {
+            Statement getLogEntry = connection.createStatement();
+            ResultSet rs = getLogEntry.executeQuery("SELECT `id`, `time`, `user`, `action`, `target`, `reason` FROM `mod_log` WHERE `server`=" + guild.getId() + " AND `id`=" + id + ";");
+
+            if (rs.next()) {
+                Instant time = Instant.ofEpochSecond(rs.getLong("time"));
+                Member user = guild.getMemberById(rs.getLong("user"));
+                User target = guild.getJDA().getUserById(rs.getLong("target"));
+
+                // Construct a user from the id
+                if (target == null) {
+                    target = new UserById(rs.getLong("target"));
+                }
+
+                return new ModLog(rs.getInt("id"), time, user, rs.getString("action"), target, rs.getString("reason"));
+            }
+
+            getLogEntry.close();
+        } catch (SQLException ignored) { }
+
+        return null;
+    }
+
+    @Override
+    public void updateLog(Guild guild, int id, String reason) {
+        try {
+            Statement updateLevelValue = connection.createStatement();
+            updateLevelValue.executeUpdate("UPDATE `mod_log` SET `reason`='" + reason + "' WHERE `id`=" + id + ";");
+            updateLevelValue.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public LevelInfo getLevel(Member user) {
+        try {
+            Statement getLevelValue = connection.createStatement();
+            ResultSet rs = getLevelValue.executeQuery("SELECT `level`, `xp`, `messages` FROM `levels` WHERE `server`=" + user.getGuild().getId() + " AND `user`=" + user.getId() + ";");
+
+            if (rs.next()) {
+                return new LevelInfo(user.getIdLong(), rs.getInt("level"), rs.getInt("xp"), rs.getInt("messages"));
+            }
+
+            getLevelValue.close();
+
+            return new LevelInfo(0, 0, 0, 0);
+        } catch (SQLException ignored) { }
+
+        return null;
+    }
+
+    @Override
+    public void setLevel(Member user, LevelInfo levelInfo) {
+        try {
+            Statement updateLevelValue = connection.createStatement();
+            updateLevelValue.executeUpdate("INSERT OR REPLACE INTO `levels` (`server`, `user`, `level`, `xp`, `messages`) VALUES (" + user.getGuild().getId() + ", " + user.getId() + ", " + levelInfo.getLevel() + ", " + levelInfo.getXp() + ", " + levelInfo.getMessages() + ");");
+            updateLevelValue.close();
+        } catch (SQLException ignored) { }
+    }
+
+    @Override
+    public List<LevelInfo> getLevels(long guild) {
+        List<LevelInfo> levels = new ArrayList<>();
+        try {
+            Statement getLevelValue = connection.createStatement();
+            ResultSet rs = getLevelValue.executeQuery("SELECT `user`, `level`, `xp`, `messages` FROM `levels` WHERE `server`=" + guild + " ORDER BY xp DESC LIMIT 100;");
+
+            while (rs.next()) {
+                levels.add(new LevelInfo(rs.getLong("user"), rs.getInt("level"), rs.getInt("xp"), rs.getInt("messages")));
+            }
+
+            getLevelValue.close();
+        } catch (SQLException ignored) { }
+
+        return levels;
+    }
+
+    @Override
+    public List<SlowModeInfo> getSlowModeChannels(Guild guild) {
+        List<SlowModeInfo> infos = new ArrayList<>();
+
+        try {
+            Statement getLogEntry = connection.createStatement();
+            ResultSet rs = getLogEntry.executeQuery("SELECT `channel`, `server`, `delay` FROM `slow_mode` WHERE `server`=" + guild.getId() + ";");
+
+            while (rs.next()) {
+                infos.add(new SlowModeInfo(rs.getLong("server"), rs.getLong("channel"), rs.getInt("delay")));
+            }
+
+            getLogEntry.close();
+        } catch (SQLException ignored) { }
+
+        return infos;
+    }
+
+    @Override
+    public void setSlowModeChannel(TextChannel channel, int delay) {
+        try {
+            Statement updateLevelValue = connection.createStatement();
+            updateLevelValue.executeUpdate("INSERT OR REPLACE INTO `slow_mode` (`channel`, `server`, `delay`) VALUES (" + channel.getId() + ", " + channel.getGuild().getId() + ", " + delay + ");");
+            updateLevelValue.close();
+        } catch (SQLException ignored) { }
     }
 }
